20cf7f9b748fdf6ae3a13d700c33737ad24e901a
liixirt <liixirt@gmail.com>
2021-02-12 13:56:48 +0100

    Modif refus stage

--- back_end/refuser-stage.php
+++ back_end/refuser-stage.php
@@ -5,14 +5,13 @@ include "../middlewares/professeur.php";
 $id_stage = $_GET['id'];
 
 // Definit l'état du stage sur RE (refus)
-// TODO: Quoi faire de l'état du stage ?
 sql_execute(
     "UPDATE stage
-SET ETAT = 'RE'
-WHERE ID_stage = :id;
+    SET ETAT = 'RE'
+    WHERE ID_stage = :id;
 ",
     ['id' => $id_stage]
 );
 
-
 header('Location: ../front_end/tdb_professeur.php');
+?>
